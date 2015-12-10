f9a785dc63b4de81786bb47f46646ccd1fc91d36
George Vigelette <gvigelet@duvitech.com>
2015-12-10 14:04:26 -0500

    Initial Commit

---
+++ MDK-ARM/RTE/RTE_Components.h
@@ -0,0 +1,14 @@
+
+/*
+ * Auto generated Run-Time-Environment Component Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'NXP_STM_Fusion' 
+ * Target:  'NXP_STM_Fusion Configuration' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+#endif /* RTE_COMPONENTS_H */
