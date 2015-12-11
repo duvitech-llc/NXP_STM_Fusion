d97aeb87fd1012d4347cf4cfeefa2a2f3bcce31c
George Vigelette <gvigelet@duvitech.com>
2015-12-11 17:04:13 -0500

    added header to file no real work done

---
+++ Inc/fusion_task.h
@@ -0,0 +1,42 @@
+/*
+  ******************************************************************************
+  * @file    fusion_task.h
+  * @author  George Vigelette
+  * @version V1.0.0
+  * @date    11 December 2015
+  * @brief   
+  ******************************************************************************
+  * @attention
+  *
+  * <h2><center>&copy; COPYRIGHT 2015 Duvitech</center></h2>
+  *
+  * Unless required by applicable law or agreed to in writing, software
+  * distributed under the License is distributed on an "AS IS" BASIS,
+  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  * See the License for the specific language governing permissions and
+  * limitations under the License.
+  *
+  ******************************************************************************
+*/
+
+/* Define to prevent recursive inclusion -------------------------------------*/
+#ifndef __FUSION_TASK_H
+#define __FUSION_TASK_H
+
+#ifdef __cplusplus
+ extern "C" {
+#endif
+
+/* Includes ------------------------------------------------------------------*/
+/* Exported types ------------------------------------------------------------*/
+/* Exported constants --------------------------------------------------------*/
+/* Exported macro ------------------------------------------------------------*/
+/* Exported functions --------------------------------------------------------*/ 
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /*__FUSION_TASK_H */
+
+/************************ (C) COPYRIGHT Duvitech *****END OF FILE****/
