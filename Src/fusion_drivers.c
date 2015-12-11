54f33ae71b4f0f036d2894d2f4facf53ebfe4a9a
George Vigelette <gvigelet@duvitech.com>
2015-12-11 01:11:01 -0500

    added driver shell should be ready to start the porting now

---
+++ Src/fusion_drivers.c
@@ -0,0 +1,33 @@
+/*
+  ******************************************************************************
+  * @file    fusion_drivers.c
+  * @author  George Vigelette
+  * @version V1.0.0
+  * @date    11 December 2015
+  * @brief   
+  ******************************************************************************
+  * @attention
+  *
+  * (C) COPYRIGHT 2015 Duvitech
+  *
+  * Unless required by applicable law or agreed to in writing, software
+  * distributed under the License is distributed on an "AS IS" BASIS,
+  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  * See the License for the specific language governing permissions and
+  * limitations under the License.
+  *
+  ******************************************************************************
+*/
+
+/* Includes ------------------------------------------------------------------*/
+#include "stm32f4xx_hal.h"
+#include "cmsis_os.h"
+
+/* Private typedef -----------------------------------------------------------*/
+/* Private define ------------------------------------------------------------*/
+/* Private macro -------------------------------------------------------------*/
+/* Private variables ---------------------------------------------------------*/
+/* Private function prototypes -----------------------------------------------*/
+/* Private functions ---------------------------------------------------------*/
+
+/************************ (C) COPYRIGHT Duvitech *****END OF FILE****/
